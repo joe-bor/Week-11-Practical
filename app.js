ae9e44969d363ea4e48bac2cc366a02b5f69830b
joe-bor <kimborlongan_08@yahoo.com>
2023-06-12 21:38:09 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const morgan = require("morgan");
+const todoRoutes = require("./routes/todoRoutes");
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(morgan("dev"));
+// app.use("todos", todoRoutes);
+
+module.exports = app;
